9d109e8a4a1ef1d560c697b394e89ad07d82193d
Crowdbotics <team@crowdbotics.com>
2024-11-13 12:36:16 +0000

    Initial commit

---
+++ backend/citrin_skillset_50022/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for citrin_skillset_50022 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "citrin_skillset_50022.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "citrin-skillset-50022",
+  url: "https://citrin-skillset-50022.botics.co",
+  api: "https://citrin-skillset-50022.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
